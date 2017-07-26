19813e090c3770ee7d8149ae10868bd4a2a77ce7
Emmanuel Adeniyi <access2emma@gmail.com>
2017-07-26 17:02:43 +0100

    Add static route to the application. Add socket.io for realtime update. Add connection and disconnection handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	})
+});
 
 
-
-
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
 	console.log(`Server Started on port: ${PORT}`);
 })
